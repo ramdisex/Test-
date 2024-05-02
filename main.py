25a6bb2e92f92eb70ca1a5f81fdcbb48afb09376
Ramu <78522777+ramdisex@users.noreply.github.com>
2024-05-03 03:07:53 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+a = 6
+b = 9
+
+print("The value of", a, "+", b, "is:", a + b)
+print("The value of", a, "-", b, "is:", a - b)
+print("The value of", a, "/", b, "is:", a / b)
+print("The value of", a, "*", b, "is:", a * b)
+print("The value of", a, "%", b, "is:", a % b)
+print("The value of", a, "//", b, "is:", a // b)
+
+print("The value of", a, "+", b, "is:", a + b)
+print("The value of", a, "-", b, "is:", a - b)
+print("The value of", a, "*", b, "is:", a * b)
+print("The value of", a, "/", b, "is:", a / b)
+print("The value of", a, "%", b, "is:", a % b)
+print("The value of", a, "//", b, "is:", a // b)
